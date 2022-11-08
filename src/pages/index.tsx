fa650b84baf3c5e9a60865384600f10ba34c4f0e
Stavros Liaskos <st.liaskos@gmail.com>
2022-11-08 17:42:44 +0100

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,69 @@
+import Head from 'next/head';
+
+import styles from '@/styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>TypeScript starter for Next.js</title>
+        <meta
+          name="description"
+          content="TypeScript starter for Next.js that includes all you need to build amazing apps"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Cabin Crew</h1>
+
+        <p className={styles.description}>
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          <i>"Cabin crew, ready for take off..."</i>
+        </p>
+
+        <div className={styles.grid}>
+          <a
+            href="https://www.instagram.com/cabincrewberlin/"
+            className={styles.card}
+          >
+            <h2>Instagram &rarr;</h2>
+          </a>
+
+          <a
+            href="https://www.facebook.com/people/Cabin-Crew/100083543341845/"
+            className={styles.card}
+          >
+            <h2>Facebook &rarr;</h2>
+          </a>
+
+          <a
+            href="https://soundcloud.com/cabin-crew-341095083"
+            className={styles.card}
+          >
+            <h2>Soundcloud &rarr;</h2>
+          </a>
+
+          {/*
+          <a
+            href="https://vercel.com/new?utm_source=typescript-nextjs-starter"
+            className={styles.card}
+          >
+            <h2>YouTube &rarr;</h2>
+          </a>
+*/}
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=typescript-nextjs-starter"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by Berlin Airlines
+        </a>
+      </footer>
+    </div>
+  );
+}
